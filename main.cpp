639e88461811847e5d8a4f3987d7af527a578f63
ChoiVoi <trinh.thequan.05@gmail.com>
2021-07-30 23:22:48 -0300

    Write half of the comments...

--- main.cpp
+++ main.cpp
@@ -5,19 +5,19 @@
 
 std::vector<int> getInput(int times)
 {
-	std::vector<int> input(times);
-	std::string buffer;
-	std::getline(std::cin, buffer);
+	std::vector<int> input(times);				//number of intergers to extract on 1 line
+	std::string buffer;							//buffer for the input string
+	std::getline(std::cin, buffer);				//get input into the buffer
 	for (int i = 0; i < times; i++)
 	{
 		if (i < times - 1)
 		{
-			input[i] = stoi(buffer.substr(0, buffer.find(" ")));
-			buffer.erase(0, buffer.find(" ") + 1);
+			input[i] = stoi(buffer.substr(0, buffer.find(" ")));		//get input from the buffer, extract up to the first " ", convert to an interger and then put into the input vector
+			buffer.erase(0, buffer.find(" ") + 1);						//erase the extracted string from buffer
 		}
 		else if (i == times -1)
 		{
-			input[i] = stoi(buffer.substr(0, buffer.length()));
+			input[i] = stoi(buffer.substr(0, buffer.length()));			//get everything that's left and put it into the input vector
 		}
 	}
 	return input;
@@ -25,35 +25,35 @@ std::vector<int> getInput(int times)
 
 std::vector<int> routesNumCal(int stationNum)
 {
-	std::vector<int> routeNum(stationNum);
-	routeNum[stationNum - 1] = 1;
+	std::vector<int> routeNum(stationNum);		//total possible routes and each number's possible routes
+	routeNum[stationNum - 1] = 1;				//hard code the first two number's possible routes
 	routeNum[stationNum - 2] = 1;
-	routeNum[0] = 2;
+	routeNum[0] = 2;							//total possible routes
 	int temp{ 1 };
-	for (int i{ stationNum - 3 }; i >= 1 ; i--)
+	for (int i{ stationNum - 3 }; i >= 1 ; i--)			//loop until there's no num left
 	{
-		temp = temp * 2;
-		routeNum[i] = temp;
-		routeNum[0] += temp;
+		temp = temp * 2;						//after the first 2 num, the possible routes doubles for each number after (ie. 1-8 = 1, 1-7 = 1, 1-6 = 2, 1-5 = 4 etc.)
+		routeNum[i] = temp;						//asign the calculated result to the corrosponding routeNum vector
+		routeNum[0] += temp;					//add temp to the total possible routes
 	}
 	return routeNum;
 }
 
 std::vector<std::vector<int>> routePlanner(std::vector<int> routeNum, int stationNum)
 {
-	std::vector<std::vector<int>> routes(routeNum[0]);
-	int routeTracker{ 0 };
-	int stationTracker{ 1 };
-	int progressTracker{ 0 };
+	std::vector<std::vector<int>> routes(routeNum[0]);		//vector to contain all possible routes
+	int routeTracker{ 0 };									//var to track the route number left
+	int stationTracker{ 1 };								//var to track which station is next
+	int progressTracker{ 0 };								//var to track if all routes has reached the last station
 
 	for (int i{ 0 }; i < routeNum[0]; i++)
 	{
-		routes[i].push_back(1);
+		routes[i].push_back(1);					//hard code asign 1 to the begining of all routes
 	}
 
-	for (int i{ 0 }; i <= routeNum[0] && progressTracker != routeNum[0]; i++)
+	for (int i{ 0 }; i <= routeNum[0] && progressTracker != routeNum[0]; i++)		//loop until all routes are recorded
 	{
-		if (i == routeNum[0])
+		if (i == routeNum[0])					//if i reached the end of the list, track the progress and asign i = 0 again
 		{
 			progressTracker = 0;
 			for (int i2{ 0 }; i2 < routeNum[0]; i2++)
@@ -63,7 +63,7 @@ std::vector<std::vector<int>> routePlanner(std::vector<int> routeNum, int statio
 			}
 			i = 0;
 		}
-		if (routes[i].back() != 8 && progressTracker != routeNum[0])
+		if (routes[i].back() != stationNum && progressTracker != routeNum[0])
 		{
 			if (routeTracker == 0)
 			{
@@ -72,7 +72,7 @@ std::vector<std::vector<int>> routePlanner(std::vector<int> routeNum, int statio
 					stationTracker = routes[i].back();
 				}
 				routeTracker = routeNum[stationTracker];
-					stationTracker += 1;
+				stationTracker += 1;
 			}
 			routes[i].push_back(stationTracker);
 			routeTracker -= 1;
